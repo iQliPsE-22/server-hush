870a94c496fbe0b277b435b03ef473fdd0d92b4a
Deepak Kumar <deepaklg02@gmail.com>
2025-02-16 15:26:39 +0530

    change in login api

--- db.js
+++ db.js
@@ -4,8 +4,6 @@ async function connectToDb() {
   await mongoose.connect(
     "mongodb+srv://iqlipse22:Uprvmz9ikr@cluster0.xkqjppx.mongodb.net/?retryWrites=true&w=majority"
   ),
-    // await mongoose.connect("mongodb://localhost:27017/user");
-
     console.log("Database connected");
 }
 
